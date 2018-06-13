83571eb0395b6f161ba8b3cee235d50356b701b6
Gavin J.W. Tian <binfront@gmail.com>
2018-06-14 00:32:00 +0800

    Create dm_settings.py

---
+++ dm_settings.py
@@ -0,0 +1,12 @@
+stock_list = ['002415', '002416', '000333']
+benchmark = '000001'
+
+init_cash = 100000
+buy_limit = 100%
+sell_limit = 100%
+
+base_commission = 5
+rate_commission = 0.0003
+
+start_date = 2016-01-01
+end_date = 2018-06-01
