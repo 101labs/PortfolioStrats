2f02d8359e5c327c9dce45bb41189f973163e632
foriyte <foriyte@163.com>
2018-06-26 01:25:07 +0800

    股票买卖函数

--- util/tradeUtil.py
+++ util/tradeUtil.py
@@ -3,6 +3,9 @@ import tushare as ts
 import datetime
 
 cal_dates = ts.trade_cal()
+commision_percent = 0.3 
+Lowbarrier = 5
+
 
 
 def is_open_day(date, region='cn'):
@@ -37,6 +40,10 @@ def get_hs300s_id():
 def get_all_stock_id():
     #To Do
     #最好可以获取全部股票id
+    stock_info = ts.get_hs300s()
+    stock_info=ts.get_stock_basics()
+    codes = list(stock_info.index.values)
+    return codes
 
 def cal_fee_cost(price, amount):
    #当前手续费0.3%，最低5元起
@@ -49,7 +56,7 @@ def cal_fee_cost(price, amount):
         else:
             return Lowbarrier
 
-def get_stock_operation_value(code, date, direction. amount):
+def get_stock_operation_value(code, date, direction,amount):
 	#对股票实行买入卖出操作
 	#code = stockid
 	#date = operation date
@@ -57,14 +64,24 @@ def get_stock_operation_value(code, date, direction. amount):
 	#amount = 股票买卖数量
 	#return = 收益或者支出,收益时为正，支出时为负,需考虑手续费
 	#To do
-	df = self.data_repository.get_onecode_df(code)
+
+	#df = self.data_repository.get_onecode_df(code)
+        df = ts.get_hist_data(code)
 	buy_signal = 0
 	buy_open_price = 0
-	if df[df['date'] == date].empty:
-		return buy_signal, buy_open_price
-        df = df[df['date'] <= date].tail(3)
-	if len(df) == 3 and df.iloc[0]['ma5'] < df.iloc[0]['ma10'] and df.iloc[1]['ma5'] > df.iloc[1]['ma10']:
-	buy_signal = 1
-	buy_open_price = df.iloc[1]['open']
-	return buy_signal, buy_open_price
+        if date not in list(df.index.values):
+	#if df[df['date'] == date].empty:
+	    return buy_signal, buy_open_price
+
+        #df = df[df['date'] <= date].tail(3)
+	#if len(df) == 3 and df.iloc[0]['ma5'] < df.iloc[0]['ma10'] and df.iloc[1]['ma5'] > df.iloc[1]['ma10']:
+	#buy_signal = 1
+	#buy_open_price = df.iloc[1]['open']
+        closeprice = df.loc[date].close
+        buy_open_price = amount*closeprice
+        if direction == 'short':
+            buy_signal = 1
+        elif direction == 'long':
+            buy_signal = -1
+	return buy_signal*buy_open_price
 
